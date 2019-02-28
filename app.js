0ccbf91e4b826d422f3ccd9cf667ee31f5ec95d4
clandaya <clandaya@scs.ryerson.ca>
2019-02-28 16:33:12 -0500

    Created landing page, campground display page, and add new campgrounds page

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+    {name: "Dawson's Creek", image: "https://images.unsplash.com/photo-1495685288924-ce05d1036b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60"},
+    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60"},
+    {name: "Dawson's Creek", image: "https://images.unsplash.com/photo-1495685288924-ce05d1036b24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
+    {name: "Granite Hill", image: "https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60"},
+    {name: "Mountain Goat's Rest", image: "https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60"}
+]
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+    res.render("campgrounds", {campgrounds : campgrounds});
+});
+
+
+app.post("/campgrounds", function(req, res){
+    //get data from the form and add campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name : name, image : image};
+    campgrounds.push(newCampground);
+    //redirect back to campgrounds page
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+});
+
+app.listen(3000, 'localhost', function(){
+    console.log("The Campground server has started!");
+});
